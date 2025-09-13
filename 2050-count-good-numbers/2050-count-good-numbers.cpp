355ca351ea2bed7c83c1368be6be91e7797f7257
Aekeesh4 <aekeeshjaiswal@gmail.com>
2025-09-14 00:20:05 +0530

    Time: 0 ms (100%), Space: 8 MB (36.99%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,23 +1,30 @@
 class Solution {
-    long long modExp(long long base, long long exp, int MOD) {
-        long long result=1;
-        base%=MOD;
-        while(exp>0) {
-            if (exp%2==1) {
-                result=(result*base)%MOD;
-            }
-            base=(base*base)%MOD;
-            exp/=2;
-        }
-        return result;
-    }
 public:
+  long long mod=1e9+7;
+  long long mypow(long long base,long long exp,long long mod){
+    long long result=1;
+    base%=mod;
+   
+    while(exp>0){
+         if(exp%2!=0)
+        result=(result*base)%mod;
+        base=(base*base)%mod;
+        exp=exp/2;
+    }
+    return result;
+  }
+   
     int countGoodNumbers(long long n) {
-        const int MOD = 1000000007;
-        long long evenCount = (n+1)/2;
-        long long oddCount = (n/2);
-        long long part1 = modExp(5, evenCount, MOD);
-        long long part2 = modExp(4, oddCount, MOD);
-        return (part1*part2)%MOD;
+        long long k=(n+1)/2;
+        long long m=n/2;
+        long long s1=mypow(5,k,mod);
+        long long s2=mypow(4,m,mod);
+        return (s1*s2)%mod;
+
+
+
+
+
+        
     }
 };
