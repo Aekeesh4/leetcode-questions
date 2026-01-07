5ee01555e823f87d19fa95c0f521dc556e641072
Aekeesh4 <aekeeshjaiswal@gmail.com>
2026-01-08 00:27:15 +0530

    Time: 0 ms (100%), Space: 42.4 MB (5.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,8 +2,8 @@ class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
 
-        int first = 1;  // f(1)
-        int second = 2; // f(2)
+        int first = 1;  
+        int second = 2; 
 
         for (int i = 3; i <= n; i++) {
             int third = first + second;
