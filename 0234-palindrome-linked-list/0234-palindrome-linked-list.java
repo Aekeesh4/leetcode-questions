0712c033d43dedd897c828773661679a80600f96
Aekeesh4 <aekeeshjaiswal@gmail.com>
2026-01-17 20:43:58 +0530

    Time: 4 ms (79.99%), Space: 94.4 MB (73.72%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find middle
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode secondHalf = reverseList(slow);
+
+        // Step 3: Compare halves
+        ListNode firstHalf = head;
+        ListNode tempSecond = secondHalf;
+        while (tempSecond != null) {
+            if (firstHalf.val != tempSecond.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            tempSecond = tempSecond.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
