7af8a2ab0de7cf9de992b16c128384ed24101388
Aekeesh4 <aekeeshjaiswal@gmail.com>
2026-08-12 19:17:40 +0530

    Time: 28 ms (7.13%), Space: 107.1 MB (55.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,9 +3,7 @@ class Solution {
         HashMap <Integer, Integer> map = new HashMap<>();
         for(int num: nums){
             map.put(num,map.getOrDefault(num,0)+1);
-        }
-        for(int num: nums)
-        {
+        
             if(map.get(num)>1)
             return true;
         }
