64010f31f31a833c61a3ab07e6b0f8a13fc04201
Aekeesh4 <aekeeshjaiswal@gmail.com>
2026-08-25 16:39:14 +0530

    Time: 0 ms (100%), Space: 46.7 MB (50.78%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -16,7 +16,7 @@
 class Solution {
     int count =0;
     public int kthSmallest(TreeNode root, int k) {
-        return helper(root,k).val;
+       return helper(root,k).val;
     }
     public TreeNode helper(TreeNode root,int k)
     {
@@ -24,17 +24,16 @@ class Solution {
         {
             return null;
         }
-        TreeNode left= helper(root.left,k);
+        TreeNode left=helper(root.left,k);
         if(left!=null)
         {
             return left;
         }
         count++;
-if(count ==k)
-{
-    return root;
-}
-
-return helper(root.right,k);
+        if(count==k)
+        {
+            return root;
+        }
+        return helper(root.right,k);
     }
 }
